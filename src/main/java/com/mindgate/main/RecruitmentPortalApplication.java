3fdf06c0685e3eda59a36fd992eba4e9ba8cfbe5
gokul <gokul23m2001@gmail.com>
2024-05-08 10:11:45 +0530

    fc

---
+++ src/main/java/com/mindgate/main/RecruitmentPortalApplication.java
@@ -0,0 +1,13 @@
+package com.mindgate.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentPortalApplication.class, args);
+	}
+
+}
