78dbb85d5b40ac45026f2f439e19c0a60e8dccc4
gokul <gokul23m2001@gmail.com>
2024-05-08 12:12:25 +0530

    interface file

---
+++ src/main/java/com/mindgate/main/repository/LoginDetailsRepositoryInterface.java
@@ -0,0 +1,7 @@
+package com.mindgate.main.repository;
+
+import com.mindgate.main.domain.LoginDetails;
+
+public interface LoginDetailsRepositoryInterface {
+	public String userLogin(LoginDetails loginDetails);
+}
